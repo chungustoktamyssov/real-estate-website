1dba3848ab690c56a6efa1ef130de87b84a58942
chungustoktamyssov <coktamyssovthingis@gmail.com>
2025-07-10 20:52:45 -0400

    2nd commit

--- mom-real-estate-website/src/App.jsx
+++ mom-real-estate-website/src/App.jsx
@@ -2,22 +2,16 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import BuyAndSell from './BuyAndSell.jsx'
 import MeetAssel from './MeetAssel.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Header';
-import Home from './Home';
-import BuyingProcess from './BuyingProcess';
-// ...other imports
 
 function App() {
   return (
-    <Router>
+    <div className="app-container">
       <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/buying/process" element={<BuyingProcess />} />
-        {/* Add more routes for your pages */}
-      </Routes>
-    </Router>
+      <MeetAssel />
+      <BuyAndSell />
+      <Footer />
+    </div>
   );
 }
+
 export default App;
